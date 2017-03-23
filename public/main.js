5670d7115e658aa995ab51eb97b2e7f6b24de8d1
Garret Morales <hurricanefan93@gmail.com>
2017-03-23 17:22:11 -0400

    Work complete

--- public/main.js
+++ public/main.js
@@ -1,6 +1,44 @@
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  const colordisplay = document.querySelector('div.colordisplay')
+  let hueValue = 0
+  let satValue = 50
+  let lightnessValue = 50
+  let alphaValue = 1
+  const hue = document.querySelector('input[name=hue]')
+  const hueNumber = document.querySelector('div.hueValue')
+  hue.addEventListener('input', (event) => {
+    hueValue = event.target.value
+    hueNumber.textContent = hueValue
+    updateColor()
+  })
+
+  const saturation = document.querySelector('input[name=saturation]')
+  const satNumber = document.querySelector('div.satValue')
+  saturation.addEventListener('input', (event) => {
+    satValue = event.target.value
+    satNumber.textContent = satValue + '%'
+    updateColor()
+  })
+
+  const lightness = document.querySelector('input[name=lightness]')
+  const lightnessNumber = document.querySelector('div.lightnessValue')
+  lightness.addEventListener('input', (event) => {
+    lightnessValue = event.target.value
+    lightnessNumber.textContent = lightnessValue + '%'
+    updateColor()
+  })
+
+  const alpha = document.querySelector('input[name=alpha]')
+  const alphaNumber = document.querySelector('div.alphaValue')
+  alpha.addEventListener('input', (event) => {
+    alphaValue = event.target.value / 100
+    alphaNumber.textContent = alphaValue
+    updateColor()
+  })
+
+  const updateColor = () => {
+    let color = `hsla(${hueValue}, ${satValue}%, ${lightnessValue}%, ${alphaValue})`
+    colordisplay.style.backgroundColor = color
+  }
 }
-
 document.addEventListener('DOMContentLoaded', main)
-
